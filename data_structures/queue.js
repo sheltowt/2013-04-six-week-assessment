81ac19cd003760ec8594abbb68fc547f95de80dc
Hack Reactor Students <github@hackreactor.com>
2013-06-17 11:43:20 -0700

    almost done

--- data_structures/queue.js
+++ data_structures/queue.js
@@ -1,2 +1,16 @@
 /* CODE GOES HERE
  * Don't forget newlines at the end of all files :) */
+
+var storage = [1,3,4,5,6];
+
+var queue = {
+
+  add: function(element){
+    storage.push(element);
+  },
+
+  remove: function(){
+    storage.shift();
+  }
+};
+
